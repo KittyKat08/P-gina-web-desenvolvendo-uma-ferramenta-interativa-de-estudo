178bca046bfbb43e4962541b6cef7180848d98b0
KittyKat08 <00001096166276sp@al.educacao.sp.gov.br>
2024-11-21 12:08:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Você Sabia?',
+    'Você já observou uma borboleta pousar delicadamente sobre uma flor?',
+    'Pois saiba que ela está experimentando o sabor das flores com os pés! Essa é uma ótima forma de saber qual local é seguro para pousar, se alimentar e se reproduzir.'
+)
+
+criaCartao(
+    'Você Sabia?',
+    'Você sabe para onde vai parar todas aquelas comidas que as formigas levam para o formigueiro?',
+    'As formigas são consideradas como fazendeiras do mundo animal. Elas cultivam fungos! Para se alimentar e mantêm uma verdadeira “agricultura” dentro de seus formigueiros.'
+)
+
+criaCartao(
+    'Você Sabia?',
+    'Você já se perguntou, do que que as joaninhas se alimentam?',
+    'As joaninhas são conhecidas por sua beleza e também por sua utilidade na agricultura. Elas se alimentam de pulgões e outros insetos! Elas são muito usadas para controlar o equilíbrio natural dos ecossistemas.'
+)
+
+criaCartao(
+    'Você Sabia?',
+    'Você já tentou acabar com uma barata arrancando sua cabeça?',
+    'Baratas sobrevivem mesmo se suas cabeças são arrancadas. Vivem cerca de uma semana, até que morram de fome, por não conseguirem se alimentar!'
+) 
